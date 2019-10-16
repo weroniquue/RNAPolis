6975c742a57afc8eaacefca5066e8f79de59b13e
Natalia <nayleeeet@gmail.com>
2019-10-16 13:40:03 +0200

    Add path to route

--- rnapolis-frontend/src/app/app.module.ts
+++ rnapolis-frontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing-module';
 import {MainPageComponent} from './components/main-page/main-page.component';
-import { MenuComponent } from './components/menu/menu.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {TeamPageComponent} from "./components/team-page/team-page.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    MenuComponent
+    MenuComponent,
+    TeamPageComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +21,5 @@ import { MenuComponent } from './components/menu/menu.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

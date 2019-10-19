42f41d48f1f16a1eb4aa9d0e9abe3321afe3a0f1
weroniquue <weronica97@gmail.com>
2019-10-19 16:12:26 +0200

    format

--- rnapolis-frontend/src/app/components/basic-components/delete-button/delete-button.component.spec.ts
+++ rnapolis-frontend/src/app/components/basic-components/delete-button/delete-button.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DeleteButtonComponent } from './delete-button.component';
+import {DeleteButtonComponent} from './delete-button.component';
 
 describe('DeleteButtonComponent', () => {
   let component: DeleteButtonComponent;
@@ -8,9 +8,9 @@ describe('DeleteButtonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeleteButtonComponent ]
+      declarations: [DeleteButtonComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

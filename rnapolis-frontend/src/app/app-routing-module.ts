bce66cbb3ea4b814cab853a7e51f6ade1fdd79a5
weroniquue <weronica97@gmail.com>
2019-10-24 09:47:22 +0200

    configure travis to use router on github pages

--- rnapolis-frontend/src/app/app-routing-module.ts
+++ rnapolis-frontend/src/app/app-routing-module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from './components/main-page/main-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent},
+  {path: '', component: MainPageComponent},
   // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}

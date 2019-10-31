da3920e4c5db38a2784433ef956458ed4cc3119e
Haradd <blazej.piaskowski@gmail.com>
2019-10-31 20:54:10 +0100

    add AuthenticationController with request and responses

---
+++ rnapolis-backend/src/main/java/rnapolis/controllers/AuthenticationController.java
@@ -0,0 +1,43 @@
+package rnapolis.controllers;
+
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rnapolis.requests.AuthenticationRequest;
+import rnapolis.responses.JwtAuthenticationResponse;
+import rnapolis.security.JwtTokenProvider;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+  @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody AuthenticationRequest request) {
+
+      authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword()
+            )
+        );
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtTokenProvider.generateToken(request.getUsername());
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+}
---
+++ rnapolis-backend/src/main/java/rnapolis/responses/JwtAuthenticationResponse.java
@@ -0,0 +1,26 @@
+package rnapolis.responses;
+
+public class JwtAuthenticationResponse {
+    private String token;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+}

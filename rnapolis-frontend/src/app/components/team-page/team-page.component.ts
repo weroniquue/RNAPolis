d17419a219c0d61d77acbc0c31ebbd10e5cdfbcf
Natalia <nayleeeet@gmail.com>
2019-11-02 19:18:05 +0100

    Review changes

--- rnapolis-frontend/src/app/components/team-page/team-page.component.ts
+++ rnapolis-frontend/src/app/components/team-page/team-page.component.ts
@@ -12,35 +12,29 @@ export class TeamPageComponent implements OnInit {
   team: TeamMember[];
   canEdit: boolean;
 
+  @ViewChild('autosize', {static: false}) autosize: CdkTextareaAutosize;
+
   constructor(private ngZone: NgZone) {
     this.team = [
       new TeamMember('Natalia', 'Łukasiewicz', 'student',
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium sunt nisi vitae et quia possimus unde tempora, sapiente rem',
-        '../../../../assets/random-photo-2.jpg'),
+        'assets/random-photo-2.jpg'),
       new TeamMember('Weronika', 'Orczyk', 'doktorant',
         'Nuancado suprenstreko antaŭpriskribo iel il, mis tohuo pleja iliard mo, fin ol numeralo finnlando mallongigoj. Povi povus',
-        '../../../../assets/random-photo-3.jpg'),
+        'assets/random-photo-3.jpg'),
       new TeamMember('Błażej', 'Piaskowski', 'profesor',
         'Лорем ипсум долор сит амет, идяуе тибияуе сингулис еос ут, дуо но алтера цивибус. Вереар мелиоре репудиаре ад меа, еам ад легере',
         null),
       new TeamMember('Jan', 'Kowalski', 'profesor',
         'シューリンガンのグーリンダイ、寿限無、寿限無。長久命の長助。水行末 雲来末 風来末、食う寝る処に住む処。やぶら小路の藪柑子。五劫の擦り切れ。水行末 雲来末 風来末。グーリンダイのポンポコピーのポンポコナーの。',
-        '../../../../assets/random-photo-4.jpg'),
+        'assets/random-photo-4.jpg'),
       new TeamMember('Anna', 'Nowak', 'magister',
         'Om ifi binom-li büdeds kanobs. Logonsös odalabon lü men, gö kil blunedolsöd klop temi. Tü dünanes olenükobs-li purgator güo. Lif',
-        '../../../../assets/random-photo.jpg'),
+        'assets/random-photo-10.jpg'),
       new TeamMember('Wojciech', 'Sipak', 'profesor',
         'Λορεμ ιπσθμ δολορ σιτ αμετ, προβο vιρτθτε ατ εοσ. Ναμ ιδ ηινc vιδερερ. Ετ vισ λεγιμθσ φαcιλισισ ελοqθεντιαμ. Ασσεντιορ',
         null)
     ];
-  }
-
-  @ViewChild('autosize', {static: false}) autosize: CdkTextareaAutosize;
-
-  triggerResize() {
-    // Wait for changes to be applied, then trigger textarea resize.
-    this.ngZone.onStable.pipe(take(1))
-      .subscribe(() => this.autosize.resizeToFitContent(true));
   }
 
   ngOnInit() {
@@ -48,6 +42,10 @@ export class TeamPageComponent implements OnInit {
 
   changeCanEdit() {
     this.canEdit = true;
+  }
+
+  setDefaultImage(member: TeamMember){
+    member.imagePath = "assets/not-found.jpg"
   }
 
   save() {

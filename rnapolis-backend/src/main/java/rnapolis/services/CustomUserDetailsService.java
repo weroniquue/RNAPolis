2554718be369cc1a75916b43f41612e2b4d6d34e
Haradd <blazej.piaskowski@gmail.com>
2019-11-02 12:46:49 +0100

    remove useless comment

--- rnapolis-backend/src/main/java/rnapolis/services/CustomUserDetailsService.java
+++ rnapolis-backend/src/main/java/rnapolis/services/CustomUserDetailsService.java
@@ -19,7 +19,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
-//    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                         .orElseThrow(() -> new UsernameNotFoundException("User not found"));

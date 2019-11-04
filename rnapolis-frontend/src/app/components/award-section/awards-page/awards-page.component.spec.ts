8b026c0b211eed999ed762549013d7b55db3b2f9
Natalia <nayleeeet@gmail.com>
2019-11-04 16:37:06 +0100

    Fix build

--- rnapolis-frontend/src/app/components/award-section/awards-page/awards-page.component.spec.ts
+++ rnapolis-frontend/src/app/components/award-section/awards-page/awards-page.component.spec.ts
@@ -7,6 +7,7 @@ import {MatFormFieldModule, MatInputModule} from '@angular/material';
 import {GenericButtonComponent} from '../../basic-components/generic-button/generic-button.component';
 import {DeleteButtonComponent} from '../../basic-components/delete-button/delete-button.component';
 import {FormsModule} from '@angular/forms';
+import {HeaderComponent} from '../../header/header.component';
 
 describe('AwardsPageComponent', () => {
   let component: AwardsPageComponent;
@@ -18,6 +19,7 @@ describe('AwardsPageComponent', () => {
         AwardsPageComponent,
         AwardsTimelineComponent,
         MenuComponent,
+        HeaderComponent,
         GenericButtonComponent,
         DeleteButtonComponent],
       imports: [MatInputModule, MatFormFieldModule, FormsModule]

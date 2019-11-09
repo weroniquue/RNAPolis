21cc42a7bd2c018b748b6a3d71c6cec573ed642f
Haradd <blazej.piaskowski@gmail.com>
2019-11-09 11:24:53 +0100

    replace autowired with lombok

--- rnapolis-backend/src/main/java/rnapolis/services/CustomUserDetailsService.java
+++ rnapolis-backend/src/main/java/rnapolis/services/CustomUserDetailsService.java
@@ -1,8 +1,7 @@
 package rnapolis.services;
 
-
 import java.util.HashSet;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,20 +10,21 @@ import org.springframework.stereotype.Service;
 import rnapolis.models.User;
 import rnapolis.repositories.UserRepository;
 
-
 @Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    UserRepository userRepository;
+  private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        HashSet<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user =
+        userRepository
+            .findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    HashSet<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
 
-
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
-    }
+    return new org.springframework.security.core.userdetails.User(
+        user.getUsername(), user.getPassword(), authorities);
+  }
 }

c554e4fe04e27b6de26d80168e60412f65683df7
Haradd <blazej.piaskowski@gmail.com>
2019-12-02 17:13:15 +0100

    remove console.logs

--- rnapolis-frontend/src/app/interceptors/jwt.interceptor.ts
+++ rnapolis-frontend/src/app/interceptors/jwt.interceptor.ts
@@ -10,7 +10,6 @@ export class JwtInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('interceptor');
     const currentUser = this.authenticationService.currentUserValue;
     if (currentUser && currentUser.token) {
       request = request.clone({

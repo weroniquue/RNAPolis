5b72feca37158999d6061396a844221ea13a5037
Haradd <blazej.piaskowski@gmail.com>
2019-12-17 14:47:08 +0100

    add awards controller

--- rnapolis-backend/src/main/java/rnapolis/repositories/AwardRepository.java
+++ rnapolis-backend/src/main/java/rnapolis/repositories/AwardRepository.java
@@ -1,13 +1,14 @@
 package rnapolis.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import rnapolis.models.Award;
 
 public interface AwardRepository extends MongoRepository<Award, String> {
 
-  Award findById(ObjectId id);
+  Optional<Award> findById(ObjectId id);
 
   List<Award> findAll();
 }

64d9773153488879f3391393e7a3193852ba7613
Haradd <blazej.piaskowski@gmail.com>
2019-12-28 10:44:21 +0100

    remove unused findAll

--- rnapolis-backend/src/main/java/rnapolis/repositories/AwardRepository.java
+++ rnapolis-backend/src/main/java/rnapolis/repositories/AwardRepository.java
@@ -10,7 +10,5 @@ public interface AwardRepository extends MongoRepository<Award, String> {
 
   Optional<Award> findById(ObjectId id);
 
-  List<Award> findAll();
-
   List<Award> findAllByOrderByYearDesc();
 };

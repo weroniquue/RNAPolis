14cc78441be81a1ed3e03ca94ad6c2275049203e
Haradd <blazej.piaskowski@gmail.com>
2019-12-28 12:59:10 +0100

    apply interacting with API

--- rnapolis-frontend/src/app/components/awards-timeline/awards-timeline.component.ts
+++ rnapolis-frontend/src/app/components/awards-timeline/awards-timeline.component.ts
@@ -5,6 +5,7 @@ import {ConfirmationDialogComponent} from '../basic-components/confirmation-dial
 import {EditAwardsComponent} from './edit-awards/edit-awards.component';
 import {AuthenticationService} from '../../services/authentication.service';
 import Utils from '../../services/utils';
+import {AwardsService} from '../../services/awards.service';
 
 @Component({
   selector: 'app-awards-timeline',
@@ -16,88 +17,18 @@ export class AwardsTimelineComponent implements OnInit {
   canEdit: boolean;
 
   constructor(public dialog: MatDialog,
-              public authenticationService: AuthenticationService) {
+              public authenticationService: AuthenticationService,
+              public awardsService: AwardsService) {
     this.canEdit = this.authenticationService.ifLogin;
-    this.awards = [{
-      year: 2019,
-      description: 'Rector scientific award for ' +
-        '"New computational methods in RNA structural bioinformatics" granted to Marta Szachniuk,' +
-        ' Maciej Antczak and Tomasz Zok by the Rector of Poznan University of Technology '
-    },
-      {
-        year: 2019, description: 'Scientific award of "Polityka" journal granted to ' +
-          'Maciej Antczak for outstanding achievements in the field of technical sciences '
-      },
-      {
-        year: 2019, description: 'Award for PhD thesis "Algorithmic aspects of RNA structure ' +
-          'similarity analysis" written by Tomasz Zok, awarded by the Scientific Council of the ' +
-          'Faculty of Computing, Poznan University of Technology '
-      },
-      {
-        year: 2019, description: 'Award for outstanding PhD thesis granted to ' +
-          'Tomasz Zok for PhD thesis "Algorithmic Aspects of RNA Structure Similarity Analysis" by the City of Poznan '
-      },
-      {
-        year: 2018, description: 'Scientific Award of the IBCh PAS Research ' +
-          'Council for pseudoknot-annotating algorithms selected the best experimental work of the year 2018 ' +
-          '(to Marta Szachniuk, Mariusz Popenda, Ryszard W. Adamiak)'
-      },
-      {
-        year: 2018, description: 'Scholarship of the Minister of Science and Higher' +
-          ' Education, Poland for outstanding scientific achievements in ' +
-          'structural bioinformatics granted to Michal Zurkowski'
-      },
-      {
-        year: 2017, description: 'Award for outstanding scientific ' +
-          'achievements (Computational methods to recognize and ' +
-          'model RNA 3D structures) granted to Marta Szachniuk by ' +
-          'the Polish Academy of Sciences - Division IV. Technical Sciences '
-      },
-      {
-        year: 2016, description: 'Scientific award for outstanding ' +
-          'scientific achievements in structural bioinformatics ' +
-          'granted to M. Szachniuk by the Rector of ' +
-          'Poznan University of Technology'
-      },
-      {
-        year: 2016, description: 'Scientific scholarship for outstanding ' +
-          'research achievements in the field of RNA structural bioinformatics' +
-          ' granted to Tomasz Zok by Chapter of the scholarship of the City of Poznan '
-      },
-      {
-        year: 2013, description: 'RNA FRABASE announced one of ten most ' +
-          'outstanding achievements in the Department of Computing, ' +
-          'Poznan University of Technology'
-      },
-      {
-        year: 2012, description: 'Award for the best master thesis ' +
-          'presented in the field of bioinformatics granted to Tomasz Zok ' +
-          'for MSc thesis "Molecular structure comparison in torsional angle space" ' +
-          'by the Polish Bioinformatics Society'
-      },
-      {
-        year: 2012, description: 'Scientific Award of the IBCh PAS Research Council ' +
-          'for RNAComposer selected the best experimental work of the ' +
-          'year 2012 (to Marta Szachniuk, Mariusz Popenda, Piotr Lukasiak, ' +
-          'Jacek Blazewicz, Ryszard W. Adamiak)'
-      },
-      {
-        year: 2009, description: 'Award for the best master thesis presented in ' +
-          'the field of bioinformatics granted to Marek Blazewicz' +
-          ' for MSc thesis "RNA fragments search engine - project and implementation" ' +
-          'by the Polish Bioinformatics Society '
-      },
-      {
-        year: 2008, description: 'Scientific Award of the IBCh PAS Research ' +
-          'Council for RNA FRABASE selected the best experimental work of the ' +
-          'year 2008 (to Marta Szachniuk, Mariusz Popenda, Ryszard W. Adamiak)'
-      }
-    ];
   }
 
   ngOnInit() {
     Utils.closeMenu();
     this.onScrollEvent();
+    this.awardsService.getAwards()
+    .subscribe(awards => {
+      this.awards = awards;
+    });
   }
 
   @HostListener('window:resize', [])
@@ -130,8 +61,9 @@ export class AwardsTimelineComponent implements OnInit {
 
     confirmationDialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.awards.splice(this.awards.indexOf(award), 1);
-        // TODO save data in db
+        this.awardsService.deleteAward(award.id).subscribe(response => {
+          this.awards.splice(this.awards.indexOf(award), 1);
+        });
       }
     });
   }
@@ -140,17 +72,19 @@ export class AwardsTimelineComponent implements OnInit {
     const editDialogRef = this.openAwardDialog(award);
 
     editDialogRef.afterClosed().subscribe(result => {
-      // TODO save data in db
-      this.awards[this.awards.indexOf(award)] = result;
+      this.awardsService.updateAward(result.id, result).subscribe(editedAward => {
+        this.awards[this.awards.indexOf(editedAward)] = editedAward;
+      });
     });
   }
 
   addAward() {
-    const addAwardDialogRef = this.openAwardDialog({year: null, description: ''});
-    addAwardDialogRef.afterClosed().subscribe(result => {
-      // TODO save data in db
-      if (result.year != null) {
-        this.awards.push(result);
+    const addAwardDialogRef = this.openAwardDialog({id: '', year: null, description: ''});
+    addAwardDialogRef.afterClosed().subscribe(newAward => {
+      if (newAward.year != null) {
+        this.awardsService.addAward(newAward).subscribe(createdAward => {
+          this.awards.unshift(createdAward);
+        });
       }
     });
   }

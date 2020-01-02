efe6dacb38e55b5780c40628518bafbeac0bfa33
Haradd <blazej.piaskowski@gmail.com>
2020-01-02 15:53:21 +0100

    remov handling errors from service

--- rnapolis-frontend/src/app/services/awards.service.spec.ts
+++ rnapolis-frontend/src/app/services/awards.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AwardsService } from './awards.service';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('AwardsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [],
+    imports: [HttpClientTestingModule],
+    providers: []
+  }));
 
   it('should be created', () => {
     const service: AwardsService = TestBed.get(AwardsService);
--- rnapolis-frontend/src/app/services/awards.service.ts
+++ rnapolis-frontend/src/app/services/awards.service.ts
@@ -1,7 +1,6 @@
-import {catchError, retry} from 'rxjs/internal/operators';
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {Award} from '../entity/award';
 import {environment} from '../../environments/environment';
 
@@ -14,33 +13,18 @@ export class AwardsService {
   }
 
   getAwards(): Observable<any> {
-    return this.http.get<Award>(apiUrl).pipe(
-      retry(1));
+    return this.http.get<Award>(apiUrl);
   }
 
   addAward(award: Award): Observable<Award> {
-    return this.http.post<Award>(apiUrl, award)
-    .pipe(
-      catchError(this.handleError('addSmartphone', award))
-    );
+    return this.http.post<Award>(apiUrl, award);
   }
 
   updateAward(id: any, award: Award): Observable<Award> {
-    return this.http.put<Award>(apiUrl + '/' + id, award)
-    .pipe(
-      catchError(this.handleError('addSmartphone', award))
-    );
+    return this.http.put<Award>(apiUrl + '/' + id, award);
   }
 
   deleteAward(id: any): Observable<Award> {
-    return this.http.delete<Award>(apiUrl + '/' + id)
-    .pipe();
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
+    return this.http.delete<Award>(apiUrl + '/' + id);
   }
 }

b24063945a7478ee3df859ef2ecff655e5569d04
Haradd <blazej.piaskowski@gmail.com>
2020-01-16 23:42:53 +0100

    adjust code to new importFromFile method

--- rnapolis-backend/src/main/java/rnapolis/changelogs/DatabaseChangelog.java
+++ rnapolis-backend/src/main/java/rnapolis/changelogs/DatabaseChangelog.java
@@ -5,6 +5,12 @@ import com.github.mongobee.changeset.ChangeSet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +18,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import rnapolis.models.Award;
+import rnapolis.models.Publication;
 import rnapolis.models.User;
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @ChangeLog
@@ -27,7 +28,7 @@ public class DatabaseChangelog {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseChangelog.class);
     private static final String HASH_PREFIX = "$2a";
 
-    @ChangeSet(order = "000", id = "initialUsersData", author = "", runAlways = true)
+    @ChangeSet(order = "000", id = "initialUsersData", author = "")
     public void initialUsersData(MongoTemplate mongoTemplate, Environment environment) {
         mongoTemplate.dropCollection("users");
         String user = Optional.ofNullable(environment.getProperty("admin.name"))
@@ -41,31 +42,44 @@ public class DatabaseChangelog {
     @ChangeSet(order = "001", id = "initialAwardsData", author = "")
     public void initialAwardsData(MongoTemplate mongoTemplate, Environment environment) {
         Optional.ofNullable(environment.getProperty("awards.filePath"))
-                .ifPresent(path -> importFromFile(path, mongoTemplate));
+                .ifPresent(path -> {
+                    String jsonString = importFromFile(path);
+                    List<Award> objects = GSON.fromJson(jsonString, new TypeToken<List<Award>>() {
+                    }.getType());
+                    objects.forEach(mongoTemplate::save);
+                });
     }
 
     @Profile("dev")
-    @ChangeSet(order = "002", id = "initialAwardsDataOnDev", author = "", runAlways = true)
+    @ChangeSet(order = "002", id = "initialAwardsDataOnDev", author = "")
     public void initialAwardsDataOnDev(MongoTemplate mongoTemplate, Environment environment) {
         Optional.ofNullable(environment.getProperty("awards.filePath"))
-                .ifPresent(path -> Optional.ofNullable(ClassLoader.getSystemClassLoader().getResource(path)).ifPresent(url -> {
-                    try {
-                        String absolutePath = url.toURI().getPath();
-                        importFromFile(absolutePath, mongoTemplate);
-                    } catch (URISyntaxException e) {
-                        logger.info("Can't find file {} in resources", path);
-                    }
-                }));
+            .ifPresent(path -> Optional.ofNullable(ClassLoader.getSystemClassLoader().getResource(path)).ifPresent(url -> {
+                try {
+                    String absolutePath = url.toURI().getPath();
+                    String jsonString = importFromFile(absolutePath);
+                    List<Award> objects = GSON.fromJson(jsonString, new TypeToken<List<Award>>() {
+                    }.getType());
+                    objects.forEach(mongoTemplate::save);
+                } catch (URISyntaxException e) {
+                    logger.info("Can't find file {} in resources", path);
+                }
+            }));
     }
 
-    @ChangeSet(order = "003", id = "initialPublicationsData", author = "", runAlways = true)
+    @ChangeSet(order = "003", id = "initialPublicationsData", author = "")
     public void initialPublicationsData(MongoTemplate mongoTemplate, Environment environment) {
         Optional.ofNullable(environment.getProperty("publications.filePath"))
-            .ifPresent(path -> importFromFile(path, mongoTemplate));
+            .ifPresent(path -> {
+                String jsonString = importFromFile(path);
+                List<Publication> objects = GSON.fromJson(jsonString, new TypeToken<List<Publication>>() {
+                }.getType());
+                objects.forEach(mongoTemplate::save);
+            });
     }
 
     @Profile("dev")
-    @ChangeSet(order = "004", id = "initialPublicationsDataOnDev", author = "", runAlways = true)
+    @ChangeSet(order = "004", id = "initialPublicationsDataOnDev", author = "")
     public void initialPublicationsDataOnDev(MongoTemplate mongoTemplate, Environment environment) {
         Optional.ofNullable(environment.getProperty("publications.filePath"))
             .ifPresent(path -> Optional.ofNullable(ClassLoader.getSystemClassLoader().getResource(path)).ifPresent(url -> {

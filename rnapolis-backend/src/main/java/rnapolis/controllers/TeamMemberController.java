a2e2497b0b050f1d48ea0bf8ad015d35abbcbf0a
Haradd <blazej.piaskowski@gmail.com>
2020-01-22 00:03:51 +0100

    set proper REST url naming convetion with hypens

--- rnapolis-backend/src/main/java/rnapolis/controllers/TeamMemberController.java
+++ rnapolis-backend/src/main/java/rnapolis/controllers/TeamMemberController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,7 @@ import rnapolis.models.TeamMember;
 import rnapolis.repositories.TeamMemberRepository;
 
 @RestController
-@RequestMapping("/api/teamMembers")
+@RequestMapping("/api/team-members")
 @AllArgsConstructor
 public class TeamMemberController {
 

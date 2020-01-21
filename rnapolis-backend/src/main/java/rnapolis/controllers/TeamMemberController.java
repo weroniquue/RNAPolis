6b201d8f71a8edc5a531182c943457e3240f9651
Haradd <blazej.piaskowski@gmail.com>
2020-01-22 00:26:53 +0100

    added crossOrigin

--- rnapolis-backend/src/main/java/rnapolis/controllers/TeamMemberController.java
+++ rnapolis-backend/src/main/java/rnapolis/controllers/TeamMemberController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import rnapolis.exception.ResourceNotFoundException;
 import rnapolis.models.TeamMember;
 import rnapolis.repositories.TeamMemberRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/team-members")
 @AllArgsConstructor

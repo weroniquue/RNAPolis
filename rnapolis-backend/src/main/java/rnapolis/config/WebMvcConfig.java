6d0c2ef55120a3e93c8df6821f1c5679d9859fb6
weroniquue <weronica97@gmail.com>
2020-01-21 17:55:37 +0100

    allow corsorigin

--- rnapolis-backend/src/main/java/rnapolis/config/WebMvcConfig.java
+++ rnapolis-backend/src/main/java/rnapolis/config/WebMvcConfig.java
@@ -1,4 +1,18 @@
 package rnapolis.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
 }
